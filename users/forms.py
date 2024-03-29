5a0fd5aa38be43eedd0abfc38fd7cb06ace23b2e
Ms2001ucme <113880137+Ms2001ucme@users.noreply.github.com>
2024-03-29 16:22:55 +0530

    Add files via upload

---
+++ users/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+   
+   class Meta:
+        model = Profile
+        fields = ['image']
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    email = forms.EmailField(label='Your Email', max_length=100)
+    message = forms.CharField(label='Your Message', widget=forms.Textarea)
